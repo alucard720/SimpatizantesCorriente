1a361c056c7023d188c2a8749db1715a38a93cb3
Miguel Angel Santana <ing.santana36@gmail.com>
2026-09-21 11:16:55 -0400

    Fix quotes in Content Security Policy directives

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ app.use(
   helmet({
     contentSecurityPolicy: {
     directives: {
-      connectSrc:["self", "https://challenges.cloudflare.com"],
+      connectSrc:["'self'", "https://challenges.cloudflare.com"],
       scriptSrc: ["'self'", "https://challenges.cloudflare.com"],
       frameSrc: ["'self'", "https://challenges.cloudflare.com"],
     },
